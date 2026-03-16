5f02af4e0a8070fa9234cf305f356b40088a0a60
MartinValentinov <martinval741@gmail.com>
2026-03-16 16:57:40 +0200

    Initial HTTP requests

--- pm-project/server/PM.API/Program.cs
+++ pm-project/server/PM.API/Program.cs
@@ -1,41 +1,84 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<UserDb>(opt => opt.UseInMemoryDatabase("UserDb"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "UserAPI";
+    config.Title = "UserAPI v1";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "UserAPI Swagger UI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
 }
 
-app.UseHttpsRedirection();
+var userItems = app.MapGroup("/users");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+userItems.MapGet("/", GetAllUsers);
+userItems.MapGet("/{id}", GetUser);
+userItems.MapPost("/", CreateUser);
+userItems.MapPut("/{id}", UpdateUser);
+userItems.MapDelete("/{id}", DeleteUser);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+static async Task<IResult> GetAllUsers(UserDb db)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    return TypedResults.Ok(await db.Users.ToArrayAsync());
+}
+
+static async Task<IResult> GetUser(int id, UserDb db)
+{
+    return await db.Users.FindAsync(id)
+        is User user
+            ? TypedResults.Ok(user)
+            : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateUser(User user, UserDb db)
+{
+    db.Users.Add(user);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/users/{user.Id}", user);
+}
+
+static async Task<IResult> UpdateUser(int id, User inputUser, UserDb db)
+{
+    var user = await db.Users.FindAsync(id);
+
+    if (user is null) return TypedResults.NotFound();
+
+    user.Name = inputUser.Name;
+    user.Email = inputUser.Email;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteUser(int id, UserDb db)
+{
+    if (await db.Users.FindAsync(id) is User user)
+    {
+        db.Users.Remove(user);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
 }
