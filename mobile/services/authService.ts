070071b8db611c47ca9b3855b28b7ecda564f238
YordanTsonev <yordantsonev20@gmail.com>
2026-03-23 11:01:26 +0200

    Delete src folder

--- mobile/services/authService.ts
+++ mobile/services/authService.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { API_URL } from "../constants/api";
 import { request } from "./apiClient";
 
@@ -23,3 +24,30 @@ export async function register(
 export async function getMe(token: string) {
   return request(`${API_URL}/Auth/me`, "GET", undefined, token);
 }
+=======
+import { API_URL } from "../constants/api";
+import { request } from "./apiClient";
+
+export async function login(username: string, password: string) {
+  return request(`${API_URL}/Auth/login`, "POST", {
+    username,
+    password,
+  });
+}
+
+export async function register(
+  username: string,
+  email: string,
+  password: string
+) {
+  return request(`${API_URL}/Auth/register`, "POST", {
+    username,
+    email,
+    password,
+  });
+}
+
+export async function getMe(token: string) {
+  return request(`${API_URL}/Auth/me`, "GET", undefined, token);
+}
+>>>>>>> Stashed changes
