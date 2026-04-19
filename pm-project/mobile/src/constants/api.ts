51542db0d0888e11555caf1457e832737678de63
AlexNay1383 <alexnay1383@gmail.com>
2026-04-19 22:52:02 +0300

    fix: update CORS and API URL for public access

--- pm-project/mobile/src/constants/api.ts
+++ pm-project/mobile/src/constants/api.ts
@@ -1,7 +1,6 @@
 import { Platform } from "react-native";
 
 
-const defaultHost = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+//const defaultHost = Platform.OS === "android" ? "10.0.2.2" : "localhost";
 
-export const API_URL =
-	(process.env.REACT_NATIVE_API_URL as string) || `http://${defaultHost}/api`;
+export const API_URL = "http://desktop-mvkg28e.tailfa346a.ts.net/api";
